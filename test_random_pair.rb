8af669113fa670aae28656e3a6a22c4e2abcc929
Eponick <mikehuntwife@gmail.com>
2018-06-20 18:39:10 -0400

    Test returning array, fail

--- test_random_pair.rb
+++ test_random_pair.rb
@@ -5,4 +5,9 @@ class TestRandomPair < Minitest::Test
   def test_assert_1_equals_1
     assert_equal(1, 1)
   end
+
+  def test_random_pair_returning_array
+    assert_equal(Array, random_pair([], []))
+  end
+  
 end
