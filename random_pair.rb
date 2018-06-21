e251aff7d4f550525e01858d4eaf77e633b72bae
Eponick <mikehuntwife@gmail.com>
2018-06-20 20:11:55 -0400

    Reread assignment, fixed mistake

--- random_pair.rb
+++ random_pair.rb
@@ -1,4 +1,4 @@
-def random_pair(arr1, arr2)
-  unless arr1.is_a?(Array) && arr2.is_a?(Array); return false; end
+def random_pair(name_array)
+  unless name_array.is_a?(Array); return false; end
   [[]]
 end
--- test_random_pair.rb
+++ test_random_pair.rb
@@ -7,19 +7,19 @@ class TestRandomPair < Minitest::Test
   end
 
   def test_random_pair_returning_array
-    assert_equal(Array, random_pair([], []).class)
+    assert_equal(Array, random_pair([]).class)
   end
 
   def test_random_pair_returning_nested_array
-    assert_equal(Array, random_pair([], [])[0].class)
+    assert_equal(Array, random_pair([])[0].class)
   end
 
   def test_random_pair_fails_invalid_arguments
-    assert_equal(false, random_pair(1, {}))
+    assert_equal(false, random_pair({}))
   end
 
   def test_random_pair_fails_single_invalid_argument
-    assert_equal(false, random_pair([], "s"))
+    assert_equal(false, random_pair("s"))
   end
   
 end
