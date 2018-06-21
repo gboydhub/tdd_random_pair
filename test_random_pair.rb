a321e42bfa737e27bfe0730567a2b648242925e6
Eponick <mikehuntwife@gmail.com>
2018-06-20 20:18:37 -0400

    Test passing invalid array type, pass

--- test_random_pair.rb
+++ test_random_pair.rb
@@ -7,11 +7,11 @@ class TestRandomPair < Minitest::Test
   end
 
   def test_random_pair_returning_array
-    assert_equal(Array, random_pair([]).class)
+    assert_equal(Array, random_pair(['']).class)
   end
 
   def test_random_pair_returning_nested_array
-    assert_equal(Array, random_pair([])[0].class)
+    assert_equal(Array, random_pair([''])[0].class)
   end
 
   def test_random_pair_fails_invalid_arguments
